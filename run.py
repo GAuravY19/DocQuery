80b29fb77639774a095c046eccab239554dff3a8
gaurav <gauravy1905@gmail.com>
2026-02-15 14:09:50 +0530

    Rnning changes

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from docquery.routes import app
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(debug = True, host='0.0.0.0')
+    # app.run(debug = True, host='0.0.0.0')
+    app.run()
